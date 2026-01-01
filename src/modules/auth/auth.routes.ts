50eee73bfe03a835364e5e64baf46ac96d80b581
MohammadSheakh <mohammad.sheakh@gmail.com>
2026-01-01 12:24:48 +0600

    Phone number visible in provider end for viewing a booking details

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -8,7 +8,6 @@ import auth from '../../middlewares/auth';
 import multer from "multer";
 const storage = multer.memoryStorage();
 const upload = multer({ storage: storage });
-
 const router = Router();
 
 
